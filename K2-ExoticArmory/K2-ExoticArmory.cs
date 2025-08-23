ad82d9be25d3f4345ec907bea0d7d206eac5e396
kenchiichi <kenshinii@outlook.com>
2025-08-23 17:33:39 -0600

    v0.1

--- K2-ExoticArmory/K2-ExoticArmory.cs
+++ K2-ExoticArmory/K2-ExoticArmory.cs
@@ -1,5 +1,7 @@
 ﻿using ANToolkit;
+using ANToolkit.Cam;
 using ANToolkit.Debugging;
+using ANToolkit.Effects;
 using ANToolkit.ResourceManagement;
 using ANToolkit.Save;
 using ANToolkit.UI;
@@ -24,6 +26,7 @@ using System.Text;
 using System.Xml.Serialization;
 using Unity;
 using UnityEngine;
+using CombatCharacter = Asuna.NewCombat.CombatCharacter;
 
 namespace K2ExoticArmory
 {
@@ -33,9 +36,17 @@ namespace K2ExoticArmory
         public List<string> NewItemNames = new List<string>();
         private Weapon _instance;
         public List<StatModifierInfo> StatModifierInfos;
+        public ItemVendor vendor;
+        public K2SimpleZoom.K2SZ k2SZ = new K2SimpleZoom.K2SZ(); 
 
         public void OnDialogueStarted(Dialogue dialogue) { }
-        public void OnFrame(float deltaTime) { }
+        public void OnFrame(float deltaTime) 
+        {
+            if (Input.GetKeyDown("i") && k2SZ.DetectMenu())
+            {
+                vendor.Catalogue.OpenShop();
+            }
+        }
         public void OnLevelChanged(string oldLevel, string newLevel) { }
         public void OnLineStarted(DialogueLine line) { }
         public void OnModUnLoaded()
@@ -62,6 +73,7 @@ namespace K2ExoticArmory
             {
                 string xml = reader.ReadToEnd();
                 List<CustomEquipment> customEquipments = Deserialize<List<CustomEquipment>>(xml);
+                List<ShopItemInfo> shopItems = new List<ShopItemInfo>();
 
                 foreach (CustomEquipment customEquipment in customEquipments)
                 {
@@ -69,11 +81,28 @@ namespace K2ExoticArmory
                     CustomWeapon item = CustomWeapon.CreateWeapon(customEquipment.Name);
                     NewItemNames.Add(item.Name);
 
+                    shopItems.Add(
+                        new ShopItemInfo()
+                        {
+                            Item = item,
+                            Cost = item.Price,
+                        }
+                    );
                 }
+                ItemShopCatalogue catalogue = ScriptableObject.CreateInstance<ItemShopCatalogue>();
+                catalogue.Items = shopItems;
+
+
+                vendor = new ItemVendor()
+                {
+                    Catalogue = catalogue,
+                };
             }
         }
         public class CustomWeapon : Asuna.Items.Weapon
         {
+            public bool IsLocked;
+            public int Price;
             public List<StatModifierInfo> StatModifierInfos;
             public static CustomWeapon CreateWeapon(string name)
             {
@@ -99,6 +128,10 @@ namespace K2ExoticArmory
             public string Description;
             public string AbilityTooltip;
             public string AbilityID;
+            public string AbilityName;
+            public bool IsLocked = false;
+            public int Price;
+            public AttackVFXType WeaponAttackVFXType;
             public List<string> Slots;
             public List<StatModifierInfo> StatModifierInfos;
             private CustomWeapon _instance;
@@ -108,18 +141,22 @@ namespace K2ExoticArmory
                 CustomWeapon weapon = ScriptableObject.CreateInstance<CustomWeapon>();
                 Ability ability = ANToolkit.ScriptableManagement.ScriptableManager.Get<Asuna.NewCombat.Ability>(AbilityID);
                 ANResourceSprite aNResourceSprite = modSpriteResolver.ResolveAsResource(PreviewImage);
-                
+
+
                 ability.Tooltip = AbilityTooltip;
+                ability.DisplayName = AbilityName;
 
                 aNResourceSprite.MOD_ONLY_USE = true;
 
                 weapon.Name = Name;
                 weapon.Slots.AddRange(Slots.Select((string x) => (EquipmentSlot)Enum.Parse(typeof(EquipmentSlot), x)));
-                weapon.AttackVFXType = AttackVFXType.EnergyGunBurst;
+                weapon.AttackVFXType = WeaponAttackVFXType;
                 weapon.Category = ItemCategory.Weapon;
                 weapon.Description = Description;
                 weapon.AddAbility(ability, ability.RestraintID);
                 weapon.StatModifierInfos = StatModifierInfos;
+                weapon.Price = Price;
+                weapon.IsLocked = IsLocked;
 
                 weapon.DisplaySprite = aNResourceSprite;
                 weapon.DisplaySpriteResource = aNResourceSprite;
